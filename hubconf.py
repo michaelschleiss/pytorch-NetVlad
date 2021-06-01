bbc435b3727d645c33fc4dbe2c305a888960a951
Michael Schleiss <michael.schleiss@fkie.fraunhofer.de>
2021-06-01 18:10:05 +0200

    sklearn

--- hubconf.py
+++ hubconf.py
@@ -16,7 +16,7 @@ class EmbedNet(nn.Module):
         #self.add_module('encoder', encoder)
         self.pool = netvlad.NetVLAD(num_clusters=64, dim=512, vladv2=False)
         #self.add_module('pool', net_vlad)
-        print(self.state_dict())
+        print(self.state_dict().keys())
     def forward():
         x = self.encoder(x)
         x = self.pool(x)
