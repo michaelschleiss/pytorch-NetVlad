6e14d7f3fdd1a50ae46cbcbc16fab7e4b3f87079
Michael Schleiss <michael.schleiss@fkie.fraunhofer.de>
2021-06-01 13:53:57 +0200

    Added hubconf.py

---
+++ hubconf.py
@@ -0,0 +1,16 @@
+dependencies = ['torch']
+import torch
+import netvlad
+
+def vgg16_netvlad(pretrained=False):
+    encoder = models.vgg16(pretrained=pretrained)
+    # capture only feature part and remove last relu and maxpool
+    layers = list(encoder.features.children())[:-2]
+    encoder = nn.Sequential(*layers)
+    model = nn.Module() 
+    model.add_module('encoder', encoder)
+    net_vlad = netvlad.NetVLAD(num_clusters=opt.num_clusters, dim=512, vladv2=False)
+    model.add_module('pool', net_vlad)
+    model = models.create('embednetpca', base_model, pool_layer)
+    model.load_state_dict(torch.hub.load_state_dict_from_url('https://github.com/yxgeee/OpenIBL/releases/download/v0.1.0-beta/vgg16_netvlad.pth', map_location=torch.device('cpu')))
+    return model
