c01551be6c028a109e4c44911d78286de7f28961
Michael Schleiss <michael.schleiss@fkie.fraunhofer.de>
2021-06-01 18:03:49 +0200

    sklearn

--- hubconf.py
+++ hubconf.py
@@ -4,35 +4,30 @@ from torch import nn
 import netvlad
 from torchvision import models
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+class EmbedNet(nn.Module):
+    def __init__(self):
+        super(EmbedNet, self).__init__()
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+         encoder = models.vgg16(pretrained=pretrained)
+        # capture only feature part and remove last relu and maxpool
+        layers = list(encoder.features.children())[:-2]
+        encoder = nn.Sequential(*layers)
+        self.add_module('encoder', encoder)
+        net_vlad = netvlad.NetVLAD(num_clusters=64, dim=512, vladv2=False)
+        self.add_module('pool', net_vlad)
 
 def vgg16_netvlad(pretrained=False):
-    encoder = models.vgg16(pretrained=pretrained)
-    # capture only feature part and remove last relu and maxpool
-    layers = list(encoder.features.children())[:-2]
-    encoder = nn.Sequential(*layers)
-    model = nn.Module() 
-    model.add_module('encoder', encoder)
-    net_vlad = netvlad.NetVLAD(num_clusters=64, dim=512, vladv2=False)
-    model.add_module('pool', net_vlad)
-
+    model = EmbedNet()
     resume_ckpt = torch.hub.load_state_dict_from_url('https://github.com/michaelschleiss/pytorch-NetVlad/releases/download/v1.0/vgg16_netvlad.pth.tar', map_location=torch.device('cpu'))
     model.load_state_dict(resume_ckpt['state_dict'])
-    model = nn.Sequential(list(model.children()))
+    
 
 
     return model
 
 def vgg16_netvlad_flip_v_and_h(pretrained=False):
-    encoder = models.vgg16(pretrained=pretrained)
-    # capture only feature part and remove last relu and maxpool
-    layers = list(encoder.features.children())[:-2]
-    encoder = nn.Sequential(*layers)
-    model = nn.Module() 
-    model.add_module('encoder', encoder)
-    net_vlad = netvlad.NetVLAD(num_clusters=64, dim=512, vladv2=False)
-    model.add_module('pool', net_vlad)
-    
+    model = EmbedNet()
     resume_ckpt = torch.hub.load_state_dict_from_url('https://github.com/michaelschleiss/pytorch-NetVlad/releases/download/v1.0/vgg16_netvlad_rot_query.pth.tar', map_location=torch.device('cpu'))
     from collections import OrderedDict
     new_state_dict = OrderedDict()
