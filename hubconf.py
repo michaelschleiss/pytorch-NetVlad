c5e4e5cf73fabcaea4c688ec97f7f61cb768377e
Michael Schleiss <michael.schleiss@fkie.fraunhofer.de>
2021-06-07 23:02:45 +0200

    Add equiv_3_epoch to hubconf

--- hubconf.py
+++ hubconf.py
@@ -30,7 +30,7 @@ def vgg16_netvlad(pretrained=False):
 def equiv_netvlad(pretrained=False):
     model = EmbedNet(dim=256)
     resume_ckpt = torch.hub.load_state_dict_from_url('https://github.com/michaelschleiss/pytorch-NetVlad/releases/download/v1.0/equiv_3_epochs.pth.tar', map_location=torch.device('cpu'))
-    model.load_state_dict(resume_ckpt['state_dict'])
+    model.load_state_dict(resume_ckpt['state_dict'], strict=False)
     return model
 
 def vgg16_netvlad_imagenet(pretrained=False):
