2a168a5fb902e25f497f6a1f1920d1b2e09f89b4
Michael Schleiss <michael.schleiss@fkie.fraunhofer.de>
2021-06-07 23:01:51 +0200

    Add equiv_3_epoch to hubconf

--- hubconf.py
+++ hubconf.py
@@ -5,7 +5,7 @@ import netvlad
 from torchvision import models
 
 class EmbedNet(nn.Module):
-    def __init__(self):
+    def __init__(self, dim = 512):
         super(EmbedNet, self).__init__()
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -14,7 +14,7 @@ class EmbedNet(nn.Module):
         layers = list(encoder.features.children())[:-2]
         self.encoder = nn.Sequential(*layers)
         #self.add_module('encoder', encoder)
-        self.pool = netvlad.NetVLAD(num_clusters=64, dim=512, vladv2=False)
+        self.pool = netvlad.NetVLAD(num_clusters=64, dim=256, vladv2=False)
         #self.add_module('pool', net_vlad)
     def forward(self, x):
         x = self.encoder(x)
@@ -28,7 +28,7 @@ def vgg16_netvlad(pretrained=False):
     return model
 
 def equiv_netvlad(pretrained=False):
-    model = EmbedNet()
+    model = EmbedNet(dim=256)
     resume_ckpt = torch.hub.load_state_dict_from_url('https://github.com/michaelschleiss/pytorch-NetVlad/releases/download/v1.0/equiv_3_epochs.pth.tar', map_location=torch.device('cpu'))
     model.load_state_dict(resume_ckpt['state_dict'])
     return model
