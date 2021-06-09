9431893bd5c41e984faf39007202fd9b08bc97a3
michaelschleiss <michaelschleiss@gmx.de>
2021-06-09 10:39:50 +0200

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -43,6 +43,8 @@ class EquivNoVlad(nn.Module):
     def forward(self, x):
         x = self.encoder(x)
         x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        #x =  F.normalize(x, p=2, dim=1)
         return x
 
 
